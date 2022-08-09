307cfd8b2226900655d2d967cf75d6b4637822ff
Arnab Mondal <amondal@phdata.io>
2022-08-09 17:48:50 +0530

    Fresh change

--- main.tf
+++ main.tf
@@ -11,40 +11,40 @@ provider "snowflake" {
   role="ACCOUNTADMIN"
   
 }
-resource "snowflake_database" "S_PROD_EBIZ" {
-  name= "S_PROD_EBIZ"
-  comment = "Production environemtn for EBIZ data source"
-  data_retention_time_in_days = 1
-}
+# resource "snowflake_database" "S_PROD_EBIZ" {
+#   name= "S_PROD_EBIZ"
+#   comment = "Production environemtn for EBIZ data source"
+#   data_retention_time_in_days = 1
+# }
 
-resource "snowflake_user" "NI_Test" {
-  name         = "NI_Test"
-  login_name   = "NI_Test"
-  comment      = "A user of snowflake."
-  password     = "Changeafterlogin"
-  disabled     = false
-  display_name = "NI_Test"
-  email        = "arnab.mondal@ni.com"
-  first_name   = "NI"
-  last_name    = "Test"
+# resource "snowflake_user" "NI_Test" {
+#   name         = "NI_Test"
+#   login_name   = "NI_Test"
+#   comment      = "A user of snowflake."
+#   password     = "Changeafterlogin"
+#   disabled     = false
+#   display_name = "NI_Test"
+#   email        = "arnab.mondal@ni.com"
+#   first_name   = "NI"
+#   last_name    = "Test"
 
-  default_warehouse = "S_PROD_WH"
-  default_role      = "DR_S_PROD_ADMIN"
-  must_change_password = true
-}
+#   default_warehouse = "S_PROD_WH"
+#   default_role      = "DR_S_PROD_ADMIN"
+#   must_change_password = true
+# }
 
-resource "snowflake_warehouse" "S_PROD_WH" {
-  name           = "S_PROD_WH"
-  comment        = "Production warehouse"
-  warehouse_size = "xsmall"
-  auto_suspend = 60
-  initially_suspended = true
-}
+# resource "snowflake_warehouse" "S_PROD_WH" {
+#   name           = "S_PROD_WH"
+#   comment        = "Production warehouse"
+#   warehouse_size = "xsmall"
+#   auto_suspend = 60
+#   initially_suspended = true
+# }
 
-resource "snowflake_role" "DR_S_PROD_ADMIN" {
-  name    = "DR_S_PROD_ADMIN"
-  comment = "All priviledges on S_PROD_{SOURCE}"
-}
+# resource "snowflake_role" "DR_S_PROD_ADMIN" {
+#   name    = "DR_S_PROD_ADMIN"
+#   comment = "All priviledges on S_PROD_{SOURCE}"
+# }
 
 provider "snowflake" {
   alias = "security_admin"
