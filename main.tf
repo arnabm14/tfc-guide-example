a12f4681c60b082ca28113c9408ee41e1b76510b
Arnab Mondal <amondal@phdata.io>
2022-08-09 16:22:47 +0530

    Added role in provider

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
+provider "snowflake" {
+  role="SYSADMIn"
+  
+}
 resource "snowflake_database" "S_PROD_EBIZ" {
   name= "S_PROD_EBIZ"
   comment = "Production environemtn for EBIX data source"
