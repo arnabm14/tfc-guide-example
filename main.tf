c18aafc7796be5943dd566ba28242b710a5c067a
Arnab Mondal <amondal@phdata.io>
2022-08-10 20:24:08 +0530

    Fresh start 4.2 : NI_test user added

--- main.tf
+++ main.tf
@@ -17,20 +17,20 @@ provider "snowflake" {
 #   data_retention_time_in_days = 1
 # }
 
-resource "snowflake_user" "TF" {
-  name         = "TF"
-  login_name   = "TF"
-  comment      = "TF Cloud user."
-  password     = "Mario@@14089797"
+resource "snowflake_user" "NI_Test" {
+  name         = "NI_Test"
+  login_name   = "NI_Test"
+  comment      = "A NI Test user of snowflake."
+  password     = "Changeafterlogin"
   disabled     = false
-  display_name = "TF"
+  display_name = "NI_Test"
   email        = "arnab.mondal@ni.com"
-  first_name   = "T"
-  last_name    = "F"
+  first_name   = "NI"
+  last_name    = "Test"
 
-  default_warehouse = "COMPUTE_WH"
-  default_role      = "SYSADMIN"
-  must_change_password = false
+  default_warehouse = "S_PROD_WH"
+  default_role      = "FR_S_PROD_ADMIN"
+  must_change_password = true
 }
 
 # resource "snowflake_warehouse" "S_PROD_WH" {
