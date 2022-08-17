0cef32eba417df23484ac7d8c1c951dcf009066a
Arnab Mondal <amondal@phdata.io>
2022-08-17 19:37:45 +0530

    Changed password 2

--- main.tf
+++ main.tf
@@ -253,7 +253,7 @@ resource "snowflake_user" "NI_Test" {
   name         = "NI_Test"
   login_name   = "NI_Test"
   comment      = "A NI Test user of snowflake."
-  password     = "ChangedAgain1"
+  password     = "ChangedAgain2"
   disabled     = false
   display_name = "NI_Test"
   email        = "arnavm014@gmail.com"
